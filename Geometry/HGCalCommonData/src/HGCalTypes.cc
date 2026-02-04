25316c322b150b66784d569873a966944d456143
Kevin Pedro <kpedro88@gmail.com>
2026-02-04 08:36:38 -0600

    named constants for array sizes that are checked later

--- Geometry/HGCalCommonData/src/HGCalTypes.cc
+++ Geometry/HGCalCommonData/src/HGCalTypes.cc
@@ -40,15 +40,15 @@ int32_t HGCalTypes::getUnpackedCellType6(int id) { return (id / faccell6_); }
 int32_t HGCalTypes::getUnpackedCell6(int id) { return (id % faccell6_); }
 
 int32_t HGCalTypes::layerType(int type) {
-  return ((type >= 0) && (type < 7)) ? HGCalTypes::layerType_[type] : HGCalTypes::WaferCenter;
+  return ((type >= 0) && (type < nLayerType_)) ? HGCalTypes::layerType_[type] : HGCalTypes::WaferCenter;
 }
 
 std::string HGCalTypes::layerTypeX(int32_t type) { return layerTypes_[HGCalTypes::layerType(type)]; }
 
 std::string HGCalTypes::waferType(int32_t type) {
-  return (((type >= 0) && (type < 4)) ? HGCalTypes::waferType_[type] : "Undefined");
+  return (((type >= 0) && (type < nWaferType_)) ? HGCalTypes::waferType_[type] : "Undefined");
 }
 
 std::string HGCalTypes::waferTypeX(int32_t type) {
-  return (((type >= 0) && (type < 27)) ? HGCalTypes::waferTypeX_[type] : "UnknownXX");
+  return (((type >= 0) && (type < nWaferTypeX_)) ? HGCalTypes::waferTypeX_[type] : "UnknownXX");
 }
